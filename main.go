188f81537222edff92ac122679fe6867b3e4f66d
Nazar G <b.gutlygeldiyev@munichelectrification.com>
2024-09-15 04:29:23 +0200

    everything functional

--- main.go
+++ main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 	"time"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/thedevsaddam/renderer"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -39,6 +43,20 @@ type (
 		Completed bool      `json:"completed"`
 		CreatedAt time.Time `json:"created_at"`
 	}
+
+	GetTodoResponse struct {
+		Message string `json:"message"`
+		Data    []Todo `json:"data"`
+	}
+
+	CreateTodo struct {
+		Title string `json:"title"`
+	}
+
+	UpdateTodo struct {
+		Title     string `json:"title"`
+		Completed bool   `json:"completed"`
+	}
 )
 
 func init() {
@@ -59,10 +77,165 @@ func init() {
 	db = client.Database(dbName)
 }
 
+func homeHandler(rw http.ResponseWriter, r *http.Request) {
+	filePath := "./README.md"
+	err := rnd.FileView(rw, http.StatusOK, filePath, "readme.md")
+	checkError(err)
+}
+
+func getTodos(rw http.ResponseWriter, r *http.Request) {
+	var todoListFromDB = []TodoModel{}
+	filter := bson.D{}
+
+	cursor, err := db.Collection(collectionName).Find(context.Background(), filter)
+
+	if err != nil {
+		log.Printf("failed to fetch todo records from the db: %v\n", err.Error())
+		rnd.JSON(rw, http.StatusBadRequest, renderer.M{"message": "Could not fetch the todo collection", "error": err.Error()})
+
+		return
+
+	}
+
+	todoList := []Todo{}
+	if err = cursor.All(context.Background(), &todoListFromDB); err != nil {
+		checkError(err)
+	}
+
+	// loop through the database list, convert TodoModel to JSON and append to the todoList array.
+	for _, td := range todoListFromDB {
+		todoList = append(todoList, Todo{
+			ID:        td.ID.Hex(),
+			Title:     td.Title,
+			Completed: td.Completed,
+			CreatedAt: td.CreatedAt,
+		})
+	}
+	rnd.JSON(rw, http.StatusOK, GetTodoResponse{Message: "All todos retrieved", Data: todoList})
+}
+
+func createTodo(rw http.ResponseWriter, r *http.Request) {
+	var todoReq CreateTodo
+
+	if err := json.NewDecoder(r.Body).Decode(&todoReq); err != nil {
+		log.Printf("failed to decode json data: %v\n", err.Error())
+		rnd.JSON(rw, http.StatusBadRequest, renderer.M{
+			"message": "could not decode data",
+		})
+		return
+	}
+
+	if todoReq.Title == "" {
+		log.Println("no title added to response body")
+		rnd.JSON(rw, http.StatusBadRequest, renderer.M{
+			"message": "please add a title",
+		})
+		return
+	}
+
+	// create a TodoModel for adding a todo to the database
+	todoModel := TodoModel{
+		ID:        primitive.NewObjectID(),
+		Title:     todoReq.Title,
+		Completed: false,
+		CreatedAt: time.Now(),
+	}
+
+	// add the todo to the database
+	data, err := db.Collection(collectionName).InsertOne(r.Context(), todoModel)
+	if err != nil {
+		log.Printf("failed to insert data into the database: %v\n", err.Error())
+		rnd.JSON(rw, http.StatusInternalServerError, renderer.M{
+			"message": "Failed to insert data into the database",
+			"error":   err.Error(),
+		})
+		return
+	}
+	rnd.JSON(rw, http.StatusCreated, renderer.M{
+		"message": "Todo created successfully",
+		"ID":      data.InsertedID,
+	})
+}
+
+func updateTodo(rw http.ResponseWriter, r *http.Request) {
+	// get the id from the url params
+	id := strings.TrimSpace(chi.URLParam(r, "id"))
+
+	res, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		log.Printf("the id param is not a valid hex value: %v\n", err.Error())
+		rnd.JSON(rw, http.StatusBadRequest, renderer.M{
+			"message": "The id is invalid",
+			"error":   err.Error(),
+		})
+		return
+	}
+
+	var updateTodoReq UpdateTodo
+
+	if err := json.NewDecoder(r.Body).Decode(&updateTodoReq); err != nil {
+		log.Printf("failed to decode the json response body data: %v\n", err.Error())
+		rnd.JSON(rw, http.StatusBadRequest, err.Error())
+	}
+	if updateTodoReq.Title == "" {
+		rnd.JSON(rw, http.StatusBadRequest, renderer.M{
+			"message": "Title cannot be empty",
+		})
+		return
+	}
+	// update the todo in the database
+	filter := bson.M{"id": res}
+	update := bson.M{"$set": bson.M{"title": updateTodoReq.Title, "completed": updateTodoReq.Completed}}
+	data, err := db.Collection(collectionName).UpdateOne(r.Context(), filter, update)
+
+	if err != nil {
+		log.Printf("failed to update db collection: %v\n", err.Error())
+		rnd.JSON(rw, http.StatusInternalServerError, renderer.M{
+			"message": "Failed to update data in the database",
+			"error":   err.Error(),
+		})
+		return
+	}
+	rnd.JSON(rw, http.StatusOK, renderer.M{
+		"message": "Todo updated successfully",
+		"data":    data.ModifiedCount,
+	})
+}
+
+func deleteTodo(rw http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	res, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		log.Printf("invalid id: %v\n", err.Error())
+		rnd.JSON(rw, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	filter := bson.M{"id": res}
+	if data, err := db.Collection(collectionName).DeleteOne(r.Context(), filter); err != nil {
+		log.Printf("could not delete item from database: %v\n", err.Error())
+		rnd.JSON(rw, http.StatusInternalServerError, renderer.M{
+			"message": "an error occurred while deleting todo item",
+			"error":   err.Error(),
+		})
+	} else {
+		rnd.JSON(rw, http.StatusOK, renderer.M{
+			"message": "item deleted successfully",
+			"data":    data,
+		})
+	}
+}
+
 func main() {
+
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Get("/", homeHandler)
+	router.Mount("/todo", todoHandlers())
+
 	server := &http.Server{
 		Addr:         ":9000",
-		Handler:      chi.NewRouter(),
+		Handler:      router,
 		ReadTimeout:  60 * time.Second,
 		WriteTimeout: 60 * time.Second,
 	}
@@ -97,6 +270,17 @@ func main() {
 		log.Fatalf("Server shutdown failed: %v\n", err)
 	}
 	log.Println("Server shutdown gracefully")
+}
+
+func todoHandlers() http.Handler {
+	router := chi.NewRouter()
+	router.Group(func(r chi.Router) {
+		r.Get("/", getTodos)
+		r.Post("/", createTodo)
+		r.Put("/{id}", updateTodo)
+		r.Delete("/{id}", deleteTodo)
+	})
+	return router
 }
 
 func checkError(err error) {
